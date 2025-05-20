5d5010c5d275ff886c37e66f1e2740f78e647c77
Oscar Bonduelle <oscar.bonduelle@hotmail.fr>
2025-05-20 22:18:17 +0200

    Add NeoPianoArduino Mod

--- modules/juce_audio_devices/juce_audio_devices.cpp
+++ modules/juce_audio_devices/juce_audio_devices.cpp
@@ -134,7 +134,6 @@
 
  #include <juce_audio_basics/midi/juce_MidiDataConcatenator.h>
  #include "native/juce_Midi_windows.cpp"
-
  #if JUCE_ASIO
   /* This is very frustrating - we only need to use a handful of definitions from
      a couple of the header files in Steinberg's ASIO SDK, and it'd be easy to copy
